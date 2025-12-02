3210737658e8c97fddc52ec725cb8098c7d8c09a
Ansh VK <anshmalikvk@gmail.com>
2025-12-02 10:38:43 +0530

    changes

--- lib/notes/screen/editor_screen.dart
+++ lib/notes/screen/editor_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_svg/svg.dart';
 import 'package:notes_app/color/app_color.dart';
 import 'package:notes_app/core/icon_assets.dart';
 import 'package:notes_app/notes/widgets/editor_textfield_widget.dart';
-import 'package:notes_app/notes/widgets/floating_toolbar_widget.dart';
 import 'package:notes_app/notes/widgets/save_changes_dialog_widget.dart';
 
 class EditorScreen extends StatefulWidget {
@@ -18,19 +17,48 @@ class _EditorScreenState extends State<EditorScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.blackcolor,
-
-      body: Stack(
-        children: [
-          SafeArea(
-            bottom: false,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: SingleChildScrollView(
-                child: Column(
+      body: SafeArea(
+        bottom: false,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
+                    Container(
+                      width: 48,
+                      height: 48,
+                      decoration: BoxDecoration(
+                        color: AppColor.greycolor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: SvgPicture.asset(
+                          IconAssets.arrowIcon,
+                          fit: BoxFit.none,
+                        ),
+                      ),
+                    ),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
+                        Container(
+                          width: 48,
+                          height: 48,
+                          decoration: BoxDecoration(
+                            color: AppColor.greycolor,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: SvgPicture.asset(
+                            IconAssets.eyeIcon,
+                            fit: BoxFit.none,
+                          ),
+                        ),
+                        SizedBox(width: 20),
                         Container(
                           width: 48,
                           height: 48,
@@ -40,64 +68,28 @@ class _EditorScreenState extends State<EditorScreen> {
                           ),
                           child: InkWell(
                             onTap: () {
-                              Navigator.pop(context);
+                              showDialog(
+                                context: context,
+                                barrierDismissible: false,
+                                builder: (context) => SaveChangesDialog(),
+                              );
                             },
                             child: SvgPicture.asset(
-                              IconAssets.arrowIcon,
+                              IconAssets.saveIcon,
                               fit: BoxFit.none,
                             ),
                           ),
                         ),
-                        Row(
-                          children: [
-                            Container(
-                              width: 48,
-                              height: 48,
-                              decoration: BoxDecoration(
-                                color: AppColor.greycolor,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: SvgPicture.asset(
-                                IconAssets.eyeIcon,
-                                fit: BoxFit.none,
-                              ),
-                            ),
-                            SizedBox(width: 20),
-                            Container(
-                              width: 48,
-                              height: 48,
-                              decoration: BoxDecoration(
-                                color: AppColor.greycolor,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: InkWell(
-                                onTap: () {
-                                  showDialog(
-                                    context: context,
-                                    barrierDismissible: false,
-                                    builder: (context) => SaveChangesDialog(),
-                                  );
-                                },
-                                child: SvgPicture.asset(
-                                  IconAssets.saveIcon,
-                                  fit: BoxFit.none,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
                       ],
                     ),
-                    SizedBox(height: 30),
-                    EditorTextFieldWidget(),
-                    // SizedBox(height: 470),
-                    FloatingToolBarWidget(),
                   ],
                 ),
-              ),
+                SizedBox(height: 30),
+                EditorTextFieldWidget(),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
